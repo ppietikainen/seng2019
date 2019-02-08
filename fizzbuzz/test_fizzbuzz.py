f66d2cc105d533ad2ddc07315cb064b68f687816
Pekka Pietikäinen <pekka@pietikainen.org>
2019-02-08 15:15:11 +0200

    Add failing test

--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -5,6 +5,9 @@ class TestFizzBuzz(unittest.TestCase):
     def test_return_number_as_string(self):
         self.assertEqual(FizzBuzz("1"), "1")
 
+    def test_return_number_as_string(self):
+        self.assertEqual(FizzBuzz("3"), "Fizz")
+
 if __name__ == '__main__':
     unittest.main()
 
